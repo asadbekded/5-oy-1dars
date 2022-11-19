d087b77a3748a18a4cd2e7df59068fe23acb71ac
asadbekded <asadbek171@gmail.com>
2022-11-19 21:18:21 +0500

    Add new change

--- src/App.js
+++ src/App.js
@@ -1,93 +1,66 @@
 import Header from './components/Header/Header';
 import Card from './components/Card/Card';
-import Search from './components/Search/Search';
 import './App.css';
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 
 function App() {
-	const data = [
-		{
-			id: 1,
-			name: 'Germany',
-			population: '81,770,900',
-			region: 'Europe',
-			capital: 'Berlin',
-			img: 'https://flagcdn.com/de.svg',
-		},
-		{
-			id: 2,
-			name: 'United States of America',
-			population: '323,947,000',
-			region: 'Americas',
-			capital: 'Washington, D.C.',
-			img: 'https://flagcdn.com/us.svg',
-		},
-		{
-			id: 3,
-			name: 'Brazil',
-			population: '206,135,893',
-			region: 'Americas',
-			capital: 'Brasília',
-			img: 'https://flagcdn.com/br.svg',
-		},
-		{
-			id: 4,
-			name: 'Iceland',
-			population: '334,300',
-			region: ' Europe',
-			capital: 'Reykjavík',
-			img: 'https://flagcdn.com/is.svg',
-		},
-		{
-			id: 5,
-			name: 'Afghanistan',
-			population: '27,657,145',
-			region: 'Asia',
-			capital: 'Kabul',
-			img: 'https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_the_Taliban.svg',
-		},
-		{
-			id: 6,
-			name: 'Åland Islands',
-			population: '28,875',
-			region: 'Europe',
-			capital: 'Mariehamn',
-			img: 'https://flagcdn.com/ax.svg',
-		},
-		{
-			id: 7,
-			name: 'Albania',
-			population: '2,886,026',
-			region: 'Europe',
-			capital: 'Tirana',
-			img: 'https://flagcdn.com/al.svg',
-		},
-		{
-			id: 8,
-			name: 'Algeria',
-			population: '40,400,000',
-			region: 'Africa',
-			capital: 'Algiers',
-			img: 'https://flagcdn.com/dz.svg',
-		},
-	];
+	let [data, setData] = useState([]);
 
+	let [value, setValue] = useState('');
+	const getValue = (evt) => {
+		setValue(evt.target.value)
+	}
+	
+	let [selvalue, opValue] = useState('')
+
+	const getOpvalue= (evt) => {
+		opValue(evt.target.value)
+	}
+	console.log(selvalue);
+
+	
+	useEffect(() => {
+		if(value.length){
+			fetch(`https://restcountries.com/v3.1/name/` + value)
+         .then(res => res.json())
+         .then(data => setData(data));
+		}else{
+			fetch('https://restcountries.com/v3.1/all')
+         .then(response => response.json())
+         .then(data => setData(data));
+		}
+		if(selvalue.length){
+			fetch(`https://restcountries.com/v3.1/region/` + selvalue)
+         .then(res => res.json())
+         .then(data => setData(data));
+		}
+	}, [value, selvalue]);
+
+	
 	return (
 		<div className='App'>
 			<Header />
 			<main>
-				<Search />
+			   <section>
+               <div className="container">
+                  <form method="post" className="search__form">
+                     <input onChange={getValue} className = "search__inp" type="serch" placeholder="Search for a country…" />
+                     <select onClick={getOpvalue} className="search__sel">
+                       <option value="" disabled selected>Filter by Region</option>
+                       {
+								data.map((el) => (
+									<option  value={el.region}>{el.region}</option>
+								))
+							  };
+                     </select>
+                  </form>
+               </div>
+            </section>
 				<section>
 					<div className='container'>
 						<ul className='card__list'>
 							{data.map((item) => (
-                <Card
-                img={item.img}
-									name={item.name}
-									population={item.population}
-									region={item.region}
-									capital={item.capital}
-								/>
+                        <Card obj={item}/>
 							))}
 						</ul>
 					</div>
@@ -96,5 +69,4 @@ function App() {
 		</div>
 	);
 };
-
 export default App;
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,15 @@
 import "./Card.css"
-
-
-const Card = ({img, name, population, region, capital}) => {
+const Card = ({ obj }) => {
    return(
       <li className="card__item">
-         <img src={img} alt="Germany-flag" width={264} height={160}/>
+         <div className="img-box">
+           <img src={obj.flags.svg} alt="Germany-flag" width={264} height={160}/>
+         </div>
          <div className="card__box">
-            <h3 className="card__title">{name}</h3>
-            <p className="card__text"><strong className="card__str">Population : </strong>{population}</p>
-            <p className="card__text"><strong className="card__str">Region : </strong>{region}</p>
-            <p className="card__text"><strong className="card__str">Capital : </strong>{capital}</p>
+            <h3 className="card__title">{obj.name.common}</h3>
+            <p className="card__text"><strong className="card__str">Population : </strong>{obj.population}</p>
+            <p className="card__text"><strong className="card__str">Region : </strong>{obj.region}</p>
+            <p className="card__text"><strong className="card__str">Capital : </strong>{obj?.capital?.[0]}</p>
          </div>
       </li>
    );
