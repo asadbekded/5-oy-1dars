e794b652df49ffda03679c1279a64a76021612a9
asadbekded <asadbek171@gmail.com>
2022-11-24 15:42:35 +0500

    Add new change

--- src/page/Flag/Flag.js
+++ src/page/Flag/Flag.js
@@ -10,7 +10,7 @@ export const Flag = () => {
    let [data, setFlag] = useState([]);
 
    useEffect(() => {
-		fetch(`https://restcountries.com/v3.1/name/` + name)
+		fetch(`https://restcountries.com/v2/name/` + name)
          .then(res => res.json())
          .then(data => setFlag(data));
 	}, [name]);
@@ -28,27 +28,29 @@ export const Flag = () => {
                        <img className='flag-img' src={el.flags.svg} alt="Germany-flag" width={450} height={300}/>
                      </div>
                      <div className='flag-content'>
-                       <h2 className="flag__title">{el.name.common}</h2>
+                       <h2 className="flag__title">{el.name}</h2>
                        <div className='flag-box'>
                        <div>
-                        {/* <p className="card__text"><strong className="card__str">Native Name : </strong>{el.name.nativeName.nld.common}</p> */}
+                        <p className="flag__text"><strong className="flag__str">Native Name : </strong>{el.nativeName}</p>
                         <p className="flag__text"><strong className="flag__str">Population : </strong>{el.population}</p>
                         <p className="flag__text"><strong className="flag__str">Region : </strong>{el.region}</p>
                         <p className="flag__text"><strong className="flag__str">Sub Region : </strong>{el.subregion}</p>
-                        <p className="flag__text"><strong className="flag__str">Capital : </strong>{el?.capital?.[0]}</p>
+                        <p className="flag__text"><strong className="flag__str">Capital : </strong>{el.capital}</p>
                        </div>
                        <div className='fox-box'>
-                         <p className="flag__text"><strong className="flag__str">Top Level Domain : </strong>{el.tld}</p>
-                         {/* <p className="card__text"><strong className="card__str">Currencies : </strong>{el.currencies.EUR.name}</p> */}
-                         <p className="flag__text"><strong className="flag__str">Languages : </strong>{`${el.languages.nld} , ${el.languages.fra} ,${el.languages.deu}`}</p>
+                         <p className="flag__text"><strong className="flag__str">Top Level Domain : </strong>{el.alpha2Code}</p>
+                         <p className="flag__text"><strong className="flag__str">Currencies : </strong>{`${el.currencies.map(itm => (itm.symbol))} - ${el.currencies.map(itm => (itm.name))}`}</p>
+                         <p className="flag__text"><strong className="flag__str">Languages : </strong>{`${el.languages.map(item => (item.name))} , ${el.languages.map(item => (item.nativeName))}`}</p>
                        </div>
                        </div>
 
                        <div className='flagbtn-box'>
                         <h3 className='flagbtn-title'>Border Countries: </h3>
-                        <button className='flag-btn' type='button'>{el.borders[0]}</button>
-                        <button className='flag-btn' type='button'>{el.borders[1]}</button>
-                        <button className='flag-btn' type='button'>{el.borders[3]}</button>
+                        {
+                           el.borders.map(item => (
+                              <button className='flag-btn' type='button'>{item}</button>
+                           ))
+                        }
                        </div>
                      </div>
                   </li>
@@ -56,7 +58,7 @@ export const Flag = () => {
                     }
                   </ul>
                ) : (<img className='no-img' src={Anima} alt='delete.img' width={250} height={100}/>)
-            };
+            }
          
       </div>
    )
